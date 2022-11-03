e602bafc05eb37256528ea2d38718f807edc3af0
Jérôme Louradour <jlouradour@linagora.com>
2022-11-03 16:41:44 +0100

    add missing import

--- audiotrain/utils/dataset.py
+++ audiotrain/utils/dataset.py
@@ -13,6 +13,7 @@ import transformers
 
 import numpy as np
 np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning) # VisibleDeprecationWarning: Creating an ndarray from ragged nested sequences (which is a list-or-tuple of lists-or-tuples-or ndarrays with different lengths or shapes) is deprecated. If you meant to do this, you must specify 'dtype=object' when creating the ndarray.
+import torch 
 
 from envsubst import envsubst
 
