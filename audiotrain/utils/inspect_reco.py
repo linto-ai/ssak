fix computation with GPU

audiotrain/utils/inspect_reco.py
@@ -18,7 +18,7 @@ def get_trellis(emission, tokens, blank_id=0, use_max = False):
     # Trellis has extra diemsions for both time axis and tokens.
     # The extra dim for tokens represents <SoS> (start-of-sentence)
     # The extra dim for time axis is for simplification of the code.
-    trellis = torch.empty((num_frame + 1, num_tokens + 1))
+    trellis = torch.empty((num_frame + 1, num_tokens + 1)).to(emission.device)
     trellis[0, 0] = 0
     trellis[1:, 0] = torch.cumsum(emission[:, blank_id], 0)
     trellis[0, -num_tokens:] = -float("inf")