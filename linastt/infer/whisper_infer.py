f458f6627f5d6e890531baa487bbf59c9da1aff6
Jérôme Louradour <jlouradour@linagora.com>
2022-12-19 14:49:50 +0100

    more options to decode with Whisper

--- linastt/infer/whisper_infer.py
+++ linastt/infer/whisper_infer.py
@@ -12,6 +12,10 @@ def whisper_infer(
     batch_size = 1,
     device = None,
     language = "fr",
+    no_speech_threshold = 0.6,
+    logprob_threshold = -1.0,
+    compression_ratio_threshold = 2.4,
+    beam_size = None,
     sort_by_len = False,
     output_ids = False,
     log_memtime = False,
@@ -57,7 +61,13 @@ def whisper_infer(
 
         pred = []
         for audio in batch:
-            res = model.transcribe(audio, language=language, fp16 = fp16, temperature = 0.0)
+            res = model.transcribe(audio, language=language, fp16 = fp16, temperature = 0.0, beam_size = beam_size,
+                no_speech_threshold = no_speech_threshold, logprob_threshold = logprob_threshold, compression_ratio_threshold = compression_ratio_threshold
+            )
+            # Note: other interesting keys of res are:
+            #   "segments": {"start", "end", "seek", "text", "tokens", "temperature", "avg_logprob", "no_speech_prob", "compression_ratio"}
+            #   - "avg_logprob" : Average log-probability of tokens
+            #   - "no_speech_prob" : Probability of no speech activity
             pred.append(res["text"])
 
         if output_ids:
@@ -83,6 +93,10 @@ if __name__ == "__main__":
     parser.add_argument('data', help="Path to data (audio file(s) or kaldi folder(s))", nargs='+')
     parser.add_argument('--model', help=f"Size of model to use. Among : {', '.join(whisper.available_models())}.", default = "base")
     parser.add_argument('--language', help=f"Language to use. Among : {', '.join(sorted(k+'('+v+')' for k,v in whisper.tokenizer.LANGUAGES.items()))}.", default = "fr")
+    parser.add_argument('--no_speech_threshold', help="Threshold for detecting no speech activity", type=float, default=0.6)
+    parser.add_argument('--logprob_threshold', help="f the average log probability over sampled tokens is below this value, returns empty string", type=float, default=-1.0)
+    parser.add_argument('--compression_ratio_threshold', help="If the gzip compression ratio is above this value, return empty string", type=float, default=2.4)
+    parser.add_argument('--beam_size', help="Size for beam search", type=int, default=None)
     parser.add_argument('--output', help="Output path (will print on stdout by default)", default = None)
     parser.add_argument('--use_ids', help="Whether to print the id before result", default=False, action="store_true")
     parser.add_argument('--gpus', help="List of GPU index to use (starting from 0)", default= None)
@@ -102,6 +116,11 @@ if __name__ == "__main__":
 
     for reco in whisper_infer(
         args.model, args.data,
+        language = args.language,
+        no_speech_threshold = args.no_speech_threshold,
+        logprob_threshold = args.logprob_threshold,
+        compression_ratio_threshold = args.compression_ratio_threshold,
+        beam_size = args.beam_size,
         #batch_size = args.batch_size,
         #sort_by_len = args.sort_by_len,
         output_ids = args.use_ids,
