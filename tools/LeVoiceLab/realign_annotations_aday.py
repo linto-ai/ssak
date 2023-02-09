db29d22b5754448c43aa08174b57131ed9681dfc
Jeronymous <jeromelouradour@gmail.com>
2023-02-09 08:54:17 +0100

    better alignment

--- tools/LeVoiceLab/realign_annotations_aday.py
+++ tools/LeVoiceLab/realign_annotations_aday.py
@@ -80,9 +80,9 @@ def find_best_position_levenshtein(subsequence, sequence, plot = True):
 
 _step_pattern = dtw.stepPattern.StepPattern(dtw.stepPattern._c(
     1, 1, 1, -1,
-    1, 0, 0, 10.,
+    1, 0, 0, 1.,
     2, 0, 1, -1,
-    2, 0, 0, 5.,
+    2, 0, 0, 1.,
     3, 1, 0, -1,
     3, 0, 0, 1,
 ), "N+M");
@@ -95,9 +95,13 @@ def distance_matrix(words1, words2):
         assert isinstance(words1, str)
         return np.array([[0. if w1 == w2 else 1. for w2 in words2] for w1 in words1])
 
-def find_best_position_dtw(subsequence, sequence, plot = False):
+def find_best_position_dtw(subsequence, sequence, plot = False, pad = True):
     
     distances = distance_matrix(subsequence, sequence)
+
+    if pad:
+        # Add zeros before / after
+        distances = np.pad(distances, ((1,1),(0,0)))
 
     alignment = dtw.dtw(distances, step_pattern=_step_pattern)
 
@@ -114,20 +118,27 @@ def find_best_position_dtw(subsequence, sequence, plot = False):
     min_distance = np.inf
     best_a = None
     best_b = None
+    ys = alignment.index1s / l1
+    ds1 = np.abs(ys)
+    ds2 = np.abs(ys - 1)
     for a in range(0, round(l2 - l1 * min_slope)):
-        bmin = min(l2, a + round(l1 * min_slope))
+        bmin = min(l2, a + round(l1 * min_slope), a + 1)
         bmax = min(l2, max(bmin+1, a + round(l1 * max_slope)))
         for b in range(bmin, bmax):
             distance = 0
-            for x, y in zip(alignment.index2s, alignment.index1s):
-                y = y / l1
+            ds3 = np.abs(ys - (alignment.index2s - a) / (b - a))
+            for x, d1, d2, d3 in zip(alignment.index2s, ds1, ds2, ds3):
                 if x <= a:
-                    distance += abs(y) # y**2
+                    distance += d1
+                    # distance += y**4
                 elif x >= b:
-                    distance += abs(y-1) # (y-1)**2
+                    distance += d2
+                    # distance += (y-1)**4
                 else:
-                    distance += abs(y - (x-a)/(b-a)) # (y - (x-a)/(b-a))**2
+                    distance += d3
+                    # distance += (y - (x-a)/(b-a))**4
             distance /= len(alignment.index1s)
+            # distance /= (b - a)
             if distance < min_distance:
                 min_distance = distance
                 best_a = a
@@ -150,10 +161,14 @@ def find_best_position_dtw(subsequence, sequence, plot = False):
     index2s = alignment.index2s + a
     index1s = alignment.index1s
 
+    if pad:
+        index1s, index2s = zip(*[(i-1,j) for (i,j) in zip(index1s, index2s) if i > 0 and i < len(subsequence)+1])
+        index1s, index2s = np.array(index1s), np.array(index2s)
+
     if plot:
         plt.figure()
         plt.imshow(distances, aspect="auto", origin='lower') #, cmap='gray', interpolation='nearest')
-        plt.plot(index2s - a, index1s, color="red")
+        plt.plot(index2s - a, index1s + 1 if pad else 0, color="red")
 
     # Refine start
     start_indices = index2s[index1s == 0]
@@ -250,7 +265,7 @@ def realign_annotations(annot_file, word_strategy = True, plot = False, verbose 
                 for elt in elt:
                     if elt.tag == "speech":
                         if elt.text:
-                            transcript = elt.text.strip()
+                            transcript = elt.text.strip().replace("’", "'")
                             if transcript[-1] not in [".", "!", "?"]:
                                 transcript += "."
                             if not len(transcripts) or transcripts[-1] is None:
@@ -275,29 +290,29 @@ def realign_annotations(annot_file, word_strategy = True, plot = False, verbose 
 
     new_transcripts = []
 
+    # Get words and timestamps from automatic transcript
+    auto_words = []
+    auto_starts = []
+    for elt in auto_transcript:
+        assert elt.tag == "s2t"
+        word = elt.text
+        start = elt.get("datetime")
+        # convert '2019-03-14T07:12:00.410000' to absolute timing
+        try:
+            start = datetime.strptime(start, "%Y-%m-%dT%H:%M:%S.%f")
+        except ValueError:
+            start = datetime.strptime(start, "%Y-%m-%dT%H:%M:%S")
+        delta = start - auto_transcript_start
+        # concert datetime.timedelta to seconds
+        start = delta.total_seconds()
+        auto_words.append(word)
+        auto_starts.append(start)
+
     for transcript in transcripts:
 
         # Get words from manual transcript
         # (split around ' and spaces)
         words = split_around_space_and_apostrophe(transcript)
-
-        # Get words and timestamps from automatic transcript
-        auto_words = []
-        auto_starts = []
-        for elt in auto_transcript:
-            assert elt.tag == "s2t"
-            word = elt.text
-            start = elt.get("datetime")
-            # convert '2019-03-14T07:12:00.410000' to absolute timing
-            try:
-                start = datetime.strptime(start, "%Y-%m-%dT%H:%M:%S.%f")
-            except ValueError:
-                start = datetime.strptime(start, "%Y-%m-%dT%H:%M:%S")
-            delta = start - auto_transcript_start
-            # concert datetime.timedelta to seconds
-            start = delta.total_seconds()
-            auto_words.append(word)
-            auto_starts.append(start)
 
         if word_strategy:
             indices = find_best_position_dtw(
@@ -334,7 +349,7 @@ def realign_annotations(annot_file, word_strategy = True, plot = False, verbose 
                 current_segment = ""
                 current_start = start
 
-            if current_segment:
+            if current_segment and not current_segment[-1] in ["'", "-"]:
                 current_segment += " "
             current_segment += word
 
@@ -345,8 +360,16 @@ def realign_annotations(annot_file, word_strategy = True, plot = False, verbose 
                     end,
                     None,
                 ])
+                # print("NOCOMMIT", current_segment)
                 current_segment = None
-                current_start = None
+                current_start = None          
+
+        last_i = indices[-1]
+        auto_starts = auto_starts[last_i:]
+        auto_words = auto_words[last_i:]
+
+        # import pdb; pdb.set_trace()
+        # print(indices, auto_starts, auto_words)
 
         assert current_segment is None
 
@@ -386,11 +409,16 @@ def save_annotation(annot, filename):
 
 if __name__ == "__main__":
 
-    file_in = "dev-1/annotation_batch/000000.annotations.json"
-    file_out = "dev-1/annotation_new/000000.annotations.json"
+    DIRIN="/media/nas/CORPUS_PENDING/Corpus_audio/Corpus_FR/ADAY/dev-1/annotation_batch"
+    DIROUT="/media/nas/CORPUS_PENDING/Corpus_audio/Corpus_FR/ADAY/dev-1/annotation_new"
 
-    annot = realign_annotations(file_in, word_strategy = True, plot = True, verbose = True)
-    save_annotation(annot, file_out)
+    for file_in in os.listdir(DIRIN):
+
+        file_in = os.path.join(DIRIN, file_in)
+        file_out = os.path.join(DIROUT, os.path.basename(file_in))
+
+        annot = realign_annotations(file_in, word_strategy = True, plot = False, verbose = True)
+        save_annotation(annot, file_out)
 
 
 
