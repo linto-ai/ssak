8f1c9778c1847b898b6eb3b90005734abaccff73
Jeronymous <jeromelouradour@gmail.com>
2023-02-13 19:11:06 +0100

    add options

--- tools/play_segments.py
+++ tools/play_segments.py
@@ -58,10 +58,14 @@ def simple_to_dict(f):
         }]
     }
 
-def check_results(audio_file, results, min_sec = 0):
+def check_results(audio_file, results, min_sec = 0, play_segment = False):
     for i, segment in enumerate(results["segments"]):
         # print(f'{segment["text"]} : {segment["start"]}-{segment["end"]}')
         # play_audiofile(audio_file, segment["start"], segment["end"], ask_for_replay = True)
+
+        if play_segment:
+            segment["words"] = [segment]
+
         for iw, word in enumerate(segment["words"]):
 
             start = word["start"]
@@ -83,14 +87,23 @@ def check_results(audio_file, results, min_sec = 0):
 
 if __name__ == "__main__":
 
-    f = sys.argv[2]
-    audio_file = sys.argv[1]
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Play audio file using results from a segmentation into words / segments')
+    parser.add_argument('audio_file', type=str, help='Audio file')
+    parser.add_argument('results_file', type=str, help='Results file')
+    parser.add_argument('--segments', default=False, action='store_true', help='Play segments instead of words')
+    parser.add_argument('--min_sec', default=0, type=float, help='Minimum second to start playing from (default: 0)')
+    args = parser.parse_args()
+
+    audio_file = args.audio_file
+    results_file = args.results_file
 
     assert os.path.isfile(audio_file), f"Cannot find audio file {audio_file}"
-    if f.endswith(".json"):
-        results = json.load(open(f))
-    elif f.endswith(".txt"):
-        results = simple_to_dict(open(f))
+    if results_file.endswith(".json"):
+        results = json.load(open(results_file))
+    elif results_file.endswith(".txt"):
+        results = simple_to_dict(open(results_file))
         import pdb; pdb.set_trace()
 
-    check_results(audio_file, results)
+    check_results(audio_file, results, play_segment=args.segments, min_sec=args.min_sec)
