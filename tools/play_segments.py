c65de3d085ce9958c5d301f9ccd6875421ccbdb5
Jeronymous <jeromelouradour@gmail.com>
2023-03-01 14:54:52 +0100

    option to play silences as well

--- tools/play_segments.py
+++ tools/play_segments.py
@@ -6,7 +6,12 @@ from linastt.utils.player import play_audiofile
 from linastt.utils.output_format import to_linstt_transcription
 
 
-def check_results(audio_file, results, min_sec = 0, play_segment = False):
+def check_results(audio_file, results, min_sec = 0, play_segment = False, play_silences = False):
+
+    additional_commands = {"q": "quit", "s": "skip segment", 20.05: "skip forward (or rewind) to 20.05 sec"}
+
+    previous_start = 0
+
     for i, segment in enumerate(results["segments"]):
         # print(f'{segment["text"]} : {segment["start"]}-{segment["end"]}')
         # play_audiofile(audio_file, segment["start"], segment["end"], ask_for_replay = True)
@@ -19,12 +24,21 @@ def check_results(audio_file, results, min_sec = 0, play_segment = False):
             start = word["start"]
             end = word["end"]
             if end < min_sec:
+                previous_start = end
                 continue
 
-            print(f"Segment {i+1}/{len(results['segments'])}, word {iw+1}/{len(segment['words'])}")
-            print(f'{word["word"]} : {start}-{end}')
-            
-            x = play_audiofile(audio_file, start, end, additional_commands = {"q": "quit", "s": "skip segment", 20.05: "skip forward (or rewind) to 20.05 sec"})
+            if play_silences and previous_start < start:
+                print(f"Silence : {previous_start}-{start}")
+                x = play_audiofile(audio_file, previous_start, start, additional_commands = additional_commands)
+            else:
+                x = None
+            previous_start = end
+
+            if x not in ["q", "s"] and not isinstance(x, float|int):
+                print(f"Segment {i+1}/{len(results['segments'])}, word {iw+1}/{len(segment['words'])}")
+                print(f'{word["word"]} : {start}-{end}')
+                
+                x = play_audiofile(audio_file, start, end, additional_commands = additional_commands)
 
             if x == "q":
                 return
@@ -45,6 +59,7 @@ if __name__ == "__main__":
     parser.add_argument('results_file', type=str, help='Results file')
     parser.add_argument('--segments', default=False, action='store_true', help='Play segments instead of words')
     parser.add_argument('--min_sec', default=0, type=float, help='Minimum second to start playing from (default: 0)')
+    parser.add_argument('--play_silences', default=False, action="store_true", help='Play silence between words')
     args = parser.parse_args()
 
     audio_file = args.audio_file
@@ -55,4 +70,8 @@ if __name__ == "__main__":
 
     results = to_linstt_transcription(results_file)
 
-    check_results(audio_file, results, play_segment=args.segments, min_sec=args.min_sec)
+    check_results(audio_file, results,
+                  play_segment=args.segments,
+                  min_sec=args.min_sec,
+                  play_silences=args.play_silences
+                  )
