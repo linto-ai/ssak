7f20bfaf7e936194ab7c52ba9f7c153b766924d5
hedi naouara <hnaouara@gmail.com>
2023-03-15 09:52:04 +0100

    adding convertisseur script

---
+++ tools/convertisseur_vid_aud.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import subprocess
+
+def convert_video_to_audio(video_path, audio_path):
+    subprocess.call(['ffmpeg', '-y', '-i', video_path, '-ar', '16000', '-ac','1', audio_path])
+
+if __name__ == '__main__':
+    input_= sys.argv[1]
+    output_= sys.argv[2]
+    for filename in os.listdir(input_):
+        if filename.endswith(".mp4"):
+            video = os.path.join(input_, filename)
+            output_filename = os.path.splitext(filename)[0] + ".mp3"
+            if not os.path.exists(output_):
+                os.makedirs(output_)
+            audio = os.path.join(output_, output_filename)
+            convert_video_to_audio(video, audio)
