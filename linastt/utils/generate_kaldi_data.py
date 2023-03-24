ce44f9c34a7f90b5c7889e3c10c6c17ec74ceca0
hedi naouara <hnaouara@gmail.com>
2023-03-24 14:24:00 +0100

    * fixing the wav.scp (move it before the for loop) * change the utt_id * adding an id to defind each seg

--- linastt/utils/generate_kaldi_data.py
+++ linastt/utils/generate_kaldi_data.py
@@ -4,7 +4,7 @@ import argparse
 import warnings
 
 
-def generate_kaldi_data(audio_folder, transcription_folder, output_folder, extension='.mp3'):
+def generate_kaldi_data(audio_folder, transcription_folder, output_folder, extension='mp3'):
     # Check if output folder exists
     os.makedirs(output_folder, exist_ok=True)
     
@@ -26,11 +26,10 @@ def generate_kaldi_data(audio_folder, transcription_folder, output_folder, exten
             open(utt2dur_file, "w", encoding='utf-8') as utt2dur, \
             open(text_file, "w", encoding='utf-8') as txt:
 
-        utt_id = 1
+        
         for audio_file in os.listdir(audio_folder):
-            expected_extensions = [".mp3", ".wav", ".ogg"]
-            if not os.path.splitext(audio_file)[1] in expected_extensions:
-                warnings.warn(f"Ignoring {audio_file} because it is not in the expected format.", UserWarning)
+            if not audio_file.endswith(f".{extension}"):
+                warnings.warn(f"\n Ignoring {audio_file} because it is not in the expected format.",UserWarning)
                 continue
 
             audio_name = os.path.splitext(audio_file)[0]
@@ -43,31 +42,32 @@ def generate_kaldi_data(audio_folder, transcription_folder, output_folder, exten
             if not os.path.exists(transcription_path):
                 warnings.warn("The transcription path does not exist. Skipping this iteration.", UserWarning)
                 continue
-
+      
             with open(transcription_path, "r") as f:  
                 reader = csv.reader(f, delimiter=";")
                 next(reader)  # skip the first row (headers)
-
+                wav_scp.write(f"{audio_name} sox {audio_path} -t wav -r 16000  -b 16 -c 1 - |\n")
+                _id = 1
                 for row in reader:
+                    utt_id = f"{audio_name}-seg_{_id:04d}"
                     text, start, duration = row
                     start = float(start)
                     duration = float(duration)
                     end = start + duration
-                    segments.write(f"{audio_name}-seg_{utt_id} {audio_name} {start:.2f} {end:.2f}\n")
-                    wav_scp.write(f"{audio_name} sox {audio_path} -t wav -r 16000  -b 16 -c 1 - |\n")
-                    utt2spk.write(f"{audio_name}-seg_{utt_id} {audio_name}-{utt_id}\n")
-                    spk2utt.write(f"{audio_name}-seg_{utt_id} {audio_name}-{utt_id}\n")
-                    spk2gender.write(f"{audio_name}-seg_{utt_id} M\n")
-                    utt2dur.write(f"{audio_name}-seg_{utt_id} {duration}\n")
-                    txt.write(f"{audio_name}-seg_{utt_id} {text}\n")
-                    utt_id += 1
+                    segments.write(f"{utt_id} {audio_name} {start:.2f} {end:.2f}\n")
+                    utt2spk.write(f"{utt_id} {utt_id}\n")
+                    spk2utt.write(f"{utt_id} {utt_id}\n")
+                    spk2gender.write(f"{utt_id} M\n")
+                    utt2dur.write(f"{utt_id} {duration}\n")
+                    txt.write(f"{utt_id} {text}\n")
+                    _id += 1
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('audios', help="Path to folder that contain the audios", type=str)
     parser.add_argument('transcription', help="Path to folder contain the transcription",  type=str)
     parser.add_argument('output', help="Path to kaldi data folder", type=str)
-    parser.add_argument('--extension', help="The file extension should be one of:[.mp3, .wav, .ogg]",  type=str, default='.mp3')
+    parser.add_argument('--extension', help="The file extension should be one of:[.mp3, .wav, .ogg]",  type=str, default='mp3')
     args = parser.parse_args()
 
     generate_kaldi_data(args.audios, args.transcription, args.output, args.extension)
