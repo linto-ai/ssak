158b9e95e78223f9e5f7e47858e2e5393df12b43
hedi naouara <hnaouara@gmail.com>
2023-03-24 13:43:48 +0100

    * Instead of repeating "{audio name}-{start:.2f}" define a variable "utterance id" * adding a warning each time something is ignored * remove that (assuming a folder contains only audio files) * use something more general, like os.path.splitext(audio_file) in [".mp3", ".wav", ".ogg"] * add an option to the script, specifying this extension (with mp3 as a default) * check existence of input folders (transcription , audios) * adding a Warning, to guide the user if this is too frequent

--- linastt/utils/generate_kaldi_data.py
+++ linastt/utils/generate_kaldi_data.py
@@ -1,54 +1,73 @@
 import os
 import csv
 import argparse
+import warnings
 
 
-def generate_kaldi_data(audio_folder, transcription_folder, output_folder):
+def generate_kaldi_data(audio_folder, transcription_folder, output_folder, extension='.mp3'):
     # Check if output folder exists
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+    os.makedirs(output_folder, exist_ok=True)
+    
+    for folder in [audio_folder,transcription_folder]:
+        assert os.path.isdir(folder), f"Input folder {folder} does not exist."
 
-    # Loop through each audio file in the audio folder
-    for audio_file in os.listdir(audio_folder):
-        if not audio_file.endswith(".mp3"):
-            continue
-        audio_name = os.path.splitext(audio_file)[0]
-        # Remove the extension
-        audio_path = os.path.join(audio_folder, audio_file)
+    segments_file = os.path.join(output_folder, "segments")
+    wav_scp_file = os.path.join(output_folder, "wav.scp")
+    utt2spk_file = os.path.join(output_folder, "utt2spk")
+    spk2utt_file = os.path.join(output_folder, "spk2utt")
+    utt2dur_file = os.path.join(output_folder, "utt2dur")
+    spk2gender_file = os.path.join(output_folder, "spk2gender")
+    text_file = os.path.join(output_folder, "text")
+    with open(segments_file, "w", encoding='utf-8') as segments, \
+            open(wav_scp_file, "w", encoding='utf-8') as wav_scp, \
+            open(utt2spk_file, "w", encoding='utf-8') as utt2spk, \
+            open(spk2utt_file, "w", encoding='utf-8') as spk2utt, \
+            open(spk2gender_file, "w", encoding='utf-8') as spk2gender, \
+            open(utt2dur_file, "w", encoding='utf-8') as utt2dur, \
+            open(text_file, "w", encoding='utf-8') as txt:
 
-        # Check if transcription file exists for this audio file
-        transcription_path = os.path.join(transcription_folder, audio_name + ".csv")
-        print(transcription_path)
-        if not os.path.exists(transcription_path):
-            continue
+        utt_id = 1
+        for audio_file in os.listdir(audio_folder):
+            expected_extensions = [".mp3", ".wav", ".ogg"]
+            if not os.path.splitext(audio_file)[1] in expected_extensions:
+                warnings.warn(f"Ignoring {audio_file} because it is not in the expected format.", UserWarning)
+                continue
 
-        # Create the {wav.scp,text,utt2spk,segments} file
-        segments_file = os.path.join(output_folder, "segments")
-        wav_scp_file = os.path.join(output_folder, "wav.scp")
-        utt2spk_file = os.path.join(output_folder, "utt2spk")
-        spk2utt_file = os.path.join(output_folder, "spk2utt")
-        text_file = os.path.join(output_folder, "text")
-        with open(transcription_path, "r") as f:  
-            reader = csv.reader(f, delimiter=";")
-            next(reader)  # skip the first row (headers)
-            with open(segments_file, "a") as segments, open(wav_scp_file, "a") as wav_scp, open(utt2spk_file, "a") as utt2spk, open(spk2utt_file, "a") as spk2utt, open(text_file, "a") as txt:
+            audio_name = os.path.splitext(audio_file)[0]
+            # Remove the extension
+            audio_path = os.path.join(audio_folder, audio_file)
+
+            # Check if transcription file exists for this audio file
+            transcription_path = os.path.join(transcription_folder, audio_name + ".csv")
+            # print(transcription_path)
+            if not os.path.exists(transcription_path):
+                warnings.warn("The transcription path does not exist. Skipping this iteration.", UserWarning)
+                continue
+
+            with open(transcription_path, "r") as f:  
+                reader = csv.reader(f, delimiter=";")
+                next(reader)  # skip the first row (headers)
+
                 for row in reader:
-                    # print(row)
                     text, start, duration = row
                     start = float(start)
                     duration = float(duration)
                     end = start + duration
-                    segments.write(f"{audio_name}-{start:.2f} {audio_name} {start:.2f} {end:.2f}\n")
+                    segments.write(f"{audio_name}-seg_{utt_id} {audio_name} {start:.2f} {end:.2f}\n")
                     wav_scp.write(f"{audio_name} sox {audio_path} -t wav -r 16000  -b 16 -c 1 - |\n")
-                    utt2spk.write(f"{audio_name}-{start:.2f} {audio_name}-{start:.2f}\n")
-                    spk2utt.write(f"{audio_name}-{start:.2f} {audio_name}-{start:.2f}\n")
-                    txt.write(f"{audio_name}-{start:.2f} {text}\n")
+                    utt2spk.write(f"{audio_name}-seg_{utt_id} {audio_name}-{utt_id}\n")
+                    spk2utt.write(f"{audio_name}-seg_{utt_id} {audio_name}-{utt_id}\n")
+                    spk2gender.write(f"{audio_name}-seg_{utt_id} M\n")
+                    utt2dur.write(f"{audio_name}-seg_{utt_id} {duration}\n")
+                    txt.write(f"{audio_name}-seg_{utt_id} {text}\n")
+                    utt_id += 1
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('audios', help="Path to folder that contain the audios", type=str)
     parser.add_argument('transcription', help="Path to folder contain the transcription",  type=str)
     parser.add_argument('output', help="Path to kaldi data folder", type=str)
+    parser.add_argument('--extension', help="The file extension should be one of:[.mp3, .wav, .ogg]",  type=str, default='.mp3')
     args = parser.parse_args()
 
-    generate_kaldi_data(args.audios, args.transcription, args.output)
+    generate_kaldi_data(args.audios, args.transcription, args.output, args.extension)
