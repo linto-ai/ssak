1abbffa3f009df611c73ba466475572e56448583
Jeronymous <jeromelouradour@gmail.com>
2023-06-07 18:32:50 +0200

    write output files on the fly

--- tools/split_long_audios.py
+++ tools/split_long_audios.py
@@ -31,7 +31,6 @@ def split_long_audio_kaldifolder(
     verbose = True,
     debug_folder = None, # "check_audio/split",
     ):
-    global new_id2text, new_id2spk, new_id2dur, new_id2seg
     MAX_LEN_PROCESSED = 0
     MAX_LEN_PROCESSED_ = 0
     assert dirout != dirin
@@ -63,108 +62,106 @@ def split_long_audio_kaldifolder(
         with(open(dirin+"/segments")) as f:
             def parse_line(l):
                 l = l.strip()
-                id, wav, start, end = l.split(" ")
-                return id, (wav, float(start), float(end))
+                id, wav_, start_, end_ = l.split(" ")
+                return id, (wav_, float(start_), float(end_))
             id2seg = dict(parse_line(l) for l in f.readlines())
     else:
         id2seg = dict((id, (id, 0, id2dur[id])) for id in id2dur)
 
     wav2path = parse_kaldi_wavscp(dirin+"/wav.scp")
 
-    new_id2text = {}
-    new_id2spk = {}
-    new_id2dur = {}
-    new_id2seg = {}
+    global start, end
+    global f_text, f_utt2spk, f_utt2dur, f_segments
 
-    idx_processed = 0
-    for id, dur in id2dur.items():
-        if id not in id2text: continue # Removed because empty transcription
-        if id == "linagora_p2_beg--55:15_linstt_julie_spk-001_Section01_Topic-None_Turn-002_seg-0000012": continue # Crappy transcription
-        if dur <= max_duration:
-            new_id2text[id] = custom_text_normalization(id2text[id])
-            new_id2spk[id] = id2spk[id]
-            new_id2dur[id] = id2dur[id]
-            new_id2seg[id] = id2seg[id]
-            continue
-        ###### special normalizations (1/2)
-        transcript = custom_text_normalization(id2text[id])
-        all_words = transcript.split()
-        ###### special normalizations that preserve word segmentations (2/2)
-        transcript = additional_normalization(transcript)
-        wavid, start, end = id2seg[id]
-        path = wav2path[wavid]
-        audio = load_audio(path, start, end, sample_rate)
-        if verbose:
-            print(f"max processed = {MAX_LEN_PROCESSED} / {MAX_LEN_PROCESSED_}")
-            print(f"Splitting: {path} // {start}-{end} ({dur})")
-            MAX_LEN_PROCESSED = max(MAX_LEN_PROCESSED, dur, end-start)
-            MAX_LEN_PROCESSED_ = max(MAX_LEN_PROCESSED_, audio.shape[0])
-            print(f"---------> {transcript}")
-        tic = time.time()
-        labels, emission, trellis, segments, word_segments = compute_alignment(audio, transcript, model, plot = False)
-        ratio = len(audio) / (trellis.size(0) * sample_rate)
-        print(f"Alignment done in {time.time()-tic:.2f}s")
-        global index, last_start, last_end, new_transcript
-        def process():
-            global index, new_id2text, new_id2spk, new_id2dur, new_id2seg, last_start, last_end, new_transcript
-            new_id = f"{id}_cut{index:02}"
-            index += 1
-            new_start = start+last_start
-            new_end = start+last_end
-            if new_end - new_start > max_duration:
-                print(f"WARNING: GOT LONG SEQUENCE {new_end-new_start} > {max_duration}")
-            if last_end <= last_start:
-                print(f"WARNING: {new_transcript} {last_start}-{last_end} ignored")
-            else:
-                assert new_end > new_start
-                print(f"Got: {new_transcript} {last_start}-{last_end} ({new_end - new_start})")
-                new_id2text[new_id] = new_transcript
-                new_id2spk[new_id] = id2spk[id]
-                new_id2dur[new_id] = new_end - new_start
-                new_id2seg[new_id] = (wavid, new_start, new_end)
-                if debug_folder:
-                    if not os.path.isdir(debug_folder):
-                        os.makedirs(debug_folder)
-                    new_transcript_ = new_transcript.replace(" ", "_").replace("'","").replace("/","-")
-                    fname = f"{dbname}_{idx_processed:03}_{index:02}" + new_transcript_
-                    ratio = len(new_transcript_.encode("utf8"))/len(new_transcript)
-                    #fname = f"{dbname}_{idx_processed:03}_{index:02}" + slugify(new_transcript)
-                    if len(fname) > (200/ratio)-4:
-                        fname = fname[:int(200/ratio)-4-23] + "..." + fname[-20:]  
-                    sox.write(debug_folder+"/"+fname+".wav", load_audio(path, new_start, new_end, sample_rate), sample_rate)
-            last_start = last_end
-            last_end = last_start
+    with open(dirout+"/text", 'w') as f_text, \
+         open(dirout+"/utt2spk", 'w') as f_utt2spk, \
+         open(dirout+"/utt2dur", 'w') as f_utt2dur, \
+         open(dirout+"/segments", 'w') as f_segments:
+        
+        def do_flush():
+            for f in [f_text, f_utt2spk, f_utt2dur, f_segments]:
+                f.flush()
+
+        idx_processed = 0
+        for id, dur in id2dur.items():
+            if id not in id2text: continue # Removed because empty transcription
+            if id == "linagora_p2_beg--55:15_linstt_julie_spk-001_Section01_Topic-None_Turn-002_seg-0000012": continue # Crappy transcription
+            if dur <= max_duration:
+                text = custom_text_normalization(id2text[id])
+                f_text.write(f"{id} {text}\n")
+                f_utt2spk.write(f"{id} {id2spk[id]}\n")
+                f_utt2dur.write(f"{id} {id2dur[id]}\n")
+                (wav_, start_, end_) = id2seg[id]
+                f_segments.write(f"{id} {wav_} {start_} {end_}\n")
+                continue
+            ###### special normalizations (1/2)
+            transcript = custom_text_normalization(id2text[id])
+            all_words = transcript.split()
+            ###### special normalizations that preserve word segmentations (2/2)
+            transcript = additional_normalization(transcript)
+            wavid, start, end = id2seg[id]
+            path = wav2path[wavid]
+            audio = load_audio(path, start, end, sample_rate)
+            if verbose:
+                print(f"max processed = {MAX_LEN_PROCESSED} / {MAX_LEN_PROCESSED_}")
+                print(f"Splitting: {path} // {start}-{end} ({dur})")
+                MAX_LEN_PROCESSED = max(MAX_LEN_PROCESSED, dur, end-start)
+                MAX_LEN_PROCESSED_ = max(MAX_LEN_PROCESSED_, audio.shape[0])
+                print(f"---------> {transcript}")
+            tic = time.time()
+            labels, emission, trellis, segments, word_segments = compute_alignment(audio, transcript, model, plot = False)
+            ratio = len(audio) / (trellis.size(0) * sample_rate)
+            print(f"Alignment done in {time.time()-tic:.2f}s")
+            global index, last_start, last_end, new_transcript
+            def process():
+                global index, f_text, f_utt2spk, f_utt2dur, f_segments, start, end, last_start, last_end, new_transcript
+                new_id = f"{id}_cut{index:02}"
+                index += 1
+                new_start = start+last_start
+                new_end = start+last_end
+                if new_end - new_start > max_duration:
+                    print(f"WARNING: GOT LONG SEQUENCE {new_end-new_start} > {max_duration}")
+                if last_end <= last_start:
+                    print(f"WARNING: {new_transcript} {last_start}-{last_end} ignored")
+                else:
+                    assert new_end > new_start
+                    print(f"Got: {new_transcript} {last_start}-{last_end} ({new_end - new_start})")
+                    f_text.write(f"{new_id} {new_transcript}\n")
+                    f_utt2spk.write(f"{new_id} {id2spk[id]}\n")
+                    f_utt2dur.write(f"{new_id} {new_end - new_start:.3f}\n")
+                    (wav, start, end) = id2seg[id]
+                    f_segments.write(f"{new_id} {wavid} {new_start:.3f} {new_end:.3f}\n")
+                    do_flush()
+                    if debug_folder:
+                        if not os.path.isdir(debug_folder):
+                            os.makedirs(debug_folder)
+                        new_transcript_ = new_transcript.replace(" ", "_").replace("'","").replace("/","-")
+                        fname = f"{dbname}_{idx_processed:03}_{index:02}" + new_transcript_
+                        ratio = len(new_transcript_.encode("utf8"))/len(new_transcript)
+                        #fname = f"{dbname}_{idx_processed:03}_{index:02}" + slugify(new_transcript)
+                        if len(fname) > (200/ratio)-4:
+                            fname = fname[:int(200/ratio)-4-23] + "..." + fname[-20:]  
+                        sox.write(debug_folder+"/"+fname+".wav", load_audio(path, new_start, new_end, sample_rate), sample_rate)
+                last_start = last_end
+                last_end = last_start
+                new_transcript = ""
+            last_start = 0
+            last_end = 0
             new_transcript = ""
-        last_start = 0
-        last_end = 0
-        new_transcript = ""
-        index = 1
-        assert len(word_segments) == len(all_words), f"{[w.label for w in word_segments]}\n{all_words}\n{len(word_segments)} != {len(all_words)}"
-        for i, (segment, word) in enumerate(zip(word_segments, all_words)):
-            end = segment.end * ratio
-            if end - last_start > max_duration:
-                process()
-            last_end = end
+            index = 1
+            assert len(word_segments) == len(all_words), f"{[w.label for w in word_segments]}\n{all_words}\n{len(word_segments)} != {len(all_words)}"
+            for i, (segment, word) in enumerate(zip(word_segments, all_words)):
+                end = segment.end * ratio
+                if end - last_start > max_duration:
+                    process()
+                last_end = end
+                if new_transcript:
+                    new_transcript += " "
+                new_transcript += word
             if new_transcript:
-                new_transcript += " "
-            new_transcript += word
-        if new_transcript:
-            last_end = word_segments[-1].end * ratio
-            process()
-        idx_processed += 1
-
-    with open(dirout+"/text", 'w') as f:
-        for id, text in new_id2text.items():
-            f.write(f"{id} {text}\n")
-    with open(dirout+"/utt2spk", 'w') as f:
-        for id, spk in new_id2spk.items():
-            f.write(f"{id} {spk}\n")
-    with open(dirout+"/utt2dur", 'w') as f:
-        for id, dur in new_id2dur.items():
-            f.write(f"{id} {dur}\n")
-    with open(dirout+"/segments", 'w') as f:
-        for id, (wav, start, end) in new_id2seg.items():
-            f.write(f"{id} {wav} {start} {end}\n")
+                last_end = word_segments[-1].end * ratio
+                process()
+            idx_processed += 1
 
     for file in "wav.scp", "spk2gender",:
         if os.path.isfile(os.path.join(dirin, file)):
