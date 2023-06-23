1544e0e4608cabca8bb45f2b04d448d03b868e68
Jeronymous <jeromelouradour@gmail.com>
2023-06-23 18:06:43 +0200

    add option to randomize

--- tools/kaldi/kaldi_take_first.py
+++ tools/kaldi/kaldi_take_first.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import os
+import random
+
 from linastt.utils.kaldi import parse_kaldi_wavscp
 from linastt.utils.kaldi import check_kaldi_dir
 
 
-def create_cut(input_folder, output_folder, n_first):
+def create_cut(input_folder, output_folder, maximum, random_seed=None):
 
     for file in ["text", "wav.scp", "utt2dur", "spk2utt", "utt2spk", "spk2gender"]:
         assert os.path.isfile(input_folder + "/" + file), f"Missing file: {input_folder}/{file}"
@@ -16,15 +18,23 @@ def create_cut(input_folder, output_folder, n_first):
     utt_ids = []
     with open(input_folder + "/text", 'r') as f, \
         open(output_folder + "/text", 'w') as text_file:
-        for i, line in zip(range(n_first), f):
-            utt_ids.append(_get_first_field(line))
-            text_file.write(line)
+        if random_seed:
+            random.seed(random_seed)
+            lines = f.readlines()
+            random.shuffle(lines)
+            for i, line in zip(range(maximum), lines):
+                utt_ids.append(_get_first_field(line))
+                text_file.write(line)
+        else:
+            for i, line in zip(range(maximum), f):
+                utt_ids.append(_get_first_field(line))
+                text_file.write(line)
 
     if os.path.isfile(input_folder + "/segments"):
         wav_ids = []
         with open(input_folder + "/segments", 'r') as f, \
             open(output_folder + "/segments", 'w') as segments:
-            for i, line in zip(range(n_first), f):
+            for line in f:
                 id = _get_first_field(line)
                 if id in utt_ids:
                     wav_id = line.split(" ")[1]
@@ -94,7 +104,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Creates a test version of a kaldi folder: taken n first lines")
     parser.add_argument("input_folder", type=str, help="Input folder with kaldi files")
     parser.add_argument("output_folder", type=str, help="Output folder to put the cut files")
-    parser.add_argument("n_first", type=int, help="n first lines to keep")
+    parser.add_argument("maximum", type=int, help="(maximum) number of lines to keep")
+    parser.add_argument("--random_seed", default=None, type=int, help="Give a random seed to take random lines (if not specified, the first lines will be taken)")
     args = parser.parse_args()
 
-    create_cut(args.input_folder, args.output_folder, args.n_first)
+    create_cut(args.input_folder, args.output_folder, args.maximum, args.random_seed)
