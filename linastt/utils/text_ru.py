b1a3b46f1b3cb9486e9a4a722102c21114d24b3c
eleonorex <murrast@mail.ru>
2023-06-26 20:56:44 +0200

    normalisation for russian: almost ready

--- linastt/utils/text_ru.py
+++ linastt/utils/text_ru.py
@@ -1,22 +1,52 @@
 import re
-import math
-import warnings
+import cyrtranslit
+from language import check_language
+
 
 from linastt.utils.text_utils import (
-    collapse_whitespace,
-    remove_special_characters,
-    format_special_characters,
+    remove_parenthesis,
     regex_escape,
-    transliterate,
-    undigit,
     cardinal_numbers_to_letters,
-    convert_symbols_to_words,
+    convert_symbols_to_words
 )
 
 from linastt.utils.text_latin import find_acronyms, roman_to_decimal
 
 _currencies = ["€", "$", "£", "¥", "₽"]
+acronyms = []
 
+
+def fix_ordinals(text):
+    term = ['ый', "ой", "ий", "ый", "го", "ая", "ья", "ые", "ых"]
+
+    alt_roots = {
+        'один': 'перв',
+        'два': 'втор',
+        'три': 'трет',
+        'четыре': 'четверт',
+        'пять': 'пят',
+        'шесть': 'шест',
+        ' семь': 'седьм',
+        'восемь': 'восьм',
+        'девять': 'девят',
+        'десять': 'десят',
+        'десят': 'десят',
+        'дцать': 'дцат',
+        'сорок': 'сорок',
+        'сто': 'сот',
+    }
+    for num in alt_roots.keys():
+        for t in term:
+            if t == 'го':
+                if num == 'три':
+                    text = re.sub(f'{num} {t}', f'{alt_roots[num]}ьего', text)
+                else:
+                    text = re.sub(f'{num} {t}', f'{alt_roots[num]}ого', text)
+
+            else:
+                text = re.sub(f'{num} {t}', f'{alt_roots[num]}{t}', text)
+
+    return text
 
 
 def format_text_ru(text,
@@ -26,17 +56,7 @@ def format_text_ru(text,
 
     text_orig = text
 
-#TODO:
-    # remove:
-    # - punctuation
-    # - whitespaces etc
-    # -
-    # lower case
-    # digits to text
-    # latin text to russian text (use transliterate?)
-    # symbols to words
-
-    # print(f" ------ ACRONYMS ----- \n {find_acronyms(text)} \n")
+    acronyms.append(find_acronyms(text))
 
     # Reorder currencies (1,20€ -> 1 € 20)
     coma = ","
@@ -46,38 +66,31 @@ def format_text_ru(text,
             text = re.sub(r"\b(\d+)" + coma + r"(\d+)\s*" +
                           c, r"\1 " + c + r" \2", text)
 
-    # Roman digits
-    if re.search(r"[IVX]", text):
-        if lang == "en":
-            digits = re.findall(
-                r"\b(?=[XVI])M*(XX{0,3})(I[XV]|V?I{0,3})(º|ый|ом)?\b", text)
-            digits = ["".join(d) for d in digits]
-
-            if digits:
-                digits = sorted(list(set(digits)), reverse=True,
-                                key=lambda x: (len(x), x))
-                for s in digits:
-                    filtered = re.sub("[a-zèº]", "", s)        # ??
-                    ordinal = filtered != s
-                    digit = roman_to_decimal(filtered)
-                    v = undigit(str(digit), lang=lang,
-                                to="ordinal" if ordinal else "cardinal")
-                    text = re.sub(r"\b" + s + r"\b", v, text)
-
     if lower_case:
         text = text.lower()
-    if not keep_punc:
-        text = re.sub(r',|;|:|\!|\?|/|\.', ' ', text)
+
+    text = convert_symbols_to_words(text=text, lang=lang, lower_case=lower_case)
+
+    text = cardinal_numbers_to_letters(text, "ru")
 
     text = re.sub("``", "\"", text)
     text = re.sub("''", "\"", text)
     text = re.sub("-+", "-", text)  # ---- -> -
+    text = re.sub("ё", "е", text)
 
-    normalized_text = collapse_whitespace(text)
+    if not keep_punc:
+        text = re.sub(r',|;|:|"|—|\!|\?|/|\.', ' ', text)
 
+    text = remove_parenthesis(text)
+    text = fix_ordinals(text)
 
-    return normalized_text
+    # best transliteraton I could find but it is very comme ci comme ça
+    # how to transliterate words in a sentence too without iterating over all words?
+    if text:
+        if not check_language(text=text, language=lang):
+            text = cyrtranslit.to_cyrillic(text, lang)
 
+    return text
 
 
 
@@ -91,3 +104,5 @@ if __name__ == "__main__":
                 print(format_text_ru(line))
     else:
         print(format_text_ru(" ".join(sys.argv[1:])))
+
+    print(f" FOUND ACRONYMS: \n {acronyms}")
