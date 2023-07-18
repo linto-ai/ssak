fc6b11d7edc72cfcfe8fc88261f7c6c4ea742370
Jeronymous <jeromelouradour@gmail.com>
2023-07-18 09:06:13 +0200

    get video and metadata in a same bash script

--- tools/scraping/scrape_youtube_fr/scrape_youtube_2.sh
+++ tools/scraping/scrape_youtube_fr/scrape_youtube_2.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+FOLDER=YouTubeFr
+
 while [ 1 -gt 0 ];do
 
     echo "Download mp4..."
-    python3 scrape_youtube_download_audio.py || exit 1
+    python3 scrape_youtube_download_audio.py $FOLDER || exit $?
+
+    echo "Get metadata..."
+    python3 scrape_youtube_get_metadata.py \
+        $FOLDER/fr \
+        -o $FOLDER/metadata \
+        --ignore_if_exists \
+        -c yt_scrape_check.tsv \
+        -p yt_scrape_metadata.json || exit $?
+
     echo "Measure timing..."
     soxi YouTubeFr/mp3/* | tail -n 2
     sleep 3600
