fcde24b32707fd4b150cf8d8392795a10654419c
Jeronymous <jeromelouradour@gmail.com>
2023-08-29 09:24:20 +0200

    improve user-friendlyness

--- tools/clean_text_ar.py
+++ tools/clean_text_ar.py
@@ -17,16 +17,19 @@ if __name__ == "__main__":
     parser.add_argument('--keep_punc', help="Whether to keep punctuations", default= False, action="store_true")
     parser.add_argument('--keep_latin_chars', help="Whether to keep latin characters (otherwise, only arabic characters)", default= False, action="store_true")
     parser.add_argument('--bw', help="Whether to transliterate text into buckwalter encoding.", default= False, action="store_true")
-    parser.add_argument('--ignore_ids', help="Whether to ignore wav ids in input", default= False, action="store_true")
+    parser.add_argument('--ignore_first', default=0, type=int, help="Ignore the first N words (can be set to 1 to ignore the first word that can be an ID)")
 
     args = parser.parse_args()
 
     input_file = args.input
-    ignore_ids = args.ignore_ids
     if args.output:
         output_file = args.output
         if os.path.exists(output_file):
             raise RuntimeError(f"Output file {output_file} already exists")
+            # os.remove(output_file)
+        dname = os.path.dirname(output_file)
+        if dname and not os.path.isdir(dname):
+            os.makedirs(dname)
         fout = open(output_file, "a", encoding="utf-8")
     else:
         fout = sys.stdout
@@ -35,33 +38,32 @@ if __name__ == "__main__":
     # Note: This is ~10 times slower than wc -l
     #       but it's reasonnable (20 sec for ~70 000 000)
     # see https://stackoverflow.com/questions/845058/how-to-get-line-count-of-a-large-file-cheaply-in-python
-    num_lines = sum(1 for _ in open(input_file))
+    if os.path.isfile(input_file):
+        num_lines = sum(1 for _ in open(input_file))
+        gen = open(input_file, "r", encoding="utf-8")
+    else:
+        num_lines = 1
+        gen = [input_file]
         
     try:
         formatted_lines = []
-        for line in tqdm(open(input_file, "r", encoding="utf-8"), total=num_lines):
-            line = line.strip()
-            
-            if ignore_ids:
-                parts = line.split()
-                wav_id = parts[0]
-                transcription = " ".join(parts[1:])
-                formatted_line = format_text_ar(transcription, keep_punc=args.keep_punc,
-                                                keep_latin_chars=args.keep_latin_chars,
-                                                bw=args.bw)
-                formatted_line = f"{wav_id} {formatted_line}"
-            else:
-                formatted_line = format_text_ar(line, keep_punc=args.keep_punc,
-                                                keep_latin_chars=args.keep_latin_chars,
-                                                bw=args.bw)
-
-            formatted_lines.extend(formatted_line.splitlines())
-
-        if fout is not sys.stdout:
-            with open(output_file, "w", encoding="utf-8") as fout:
-                fout.write('\n'.join(formatted_lines))
-        else:
-            print('\n'.join(formatted_lines))
+        for line in tqdm(gen, total=num_lines):
+            if args.ignore_first:
+                words = line.split()
+                assert len(words) >= args.ignore_first, f"Line {line} has less than {args.ignore_first} words"
+                line = " ".join(words[args.ignore_first:])
+            line = format_text_ar(line,
+                keep_punc=args.keep_punc,
+                keep_latin_chars=args.keep_latin_chars,
+                bw=args.bw
+            )
+            for subline in line.splitlines():
+                subline = subline.strip()
+                if subline:
+                    if args.ignore_first:
+                        subline = " ".join(words[:args.ignore_first]) + " " + subline
+                    fout.write(subline+"\n")
+                    fout.flush()
     finally:
         if fout is not sys.stdout and fout is not None:
             fout.close()
