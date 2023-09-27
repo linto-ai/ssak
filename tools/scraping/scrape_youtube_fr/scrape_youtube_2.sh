19e00f6a81bdc6d4c96f73ed7bd707c86848d6e4
Jeronymous <jeromelouradour@gmail.com>
2023-09-27 08:09:30 +0200

    update scripts

--- tools/scraping/scrape_youtube_fr/scrape_youtube_2.sh
+++ tools/scraping/scrape_youtube_fr/scrape_youtube_2.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-FOLDER=YouTubeFr
+
+set -e
+
+__FILE__=`realpath $0`
+FOLDER=`dirname $__FILE__`
+FOLDER=`dirname $FOLDER`
+
+echo $FOLDER
+cd $FOLDER
+
+MAINDIR=/media/nas/CORPUS_PENDING/Corpus_audio/Corpus_FR/YouTubeFr
 
 while [ 1 -gt 0 ];do
 
     echo "Download mp4..."
-    python3 scrape_youtube_download_audio.py $FOLDER || exit $?
+    python3 scrape_youtube_download_audio.py $MAINDIR || exit $?
 
     echo "Get metadata..."
     python3 scrape_youtube_get_metadata.py \
-        $FOLDER/fr \
-        -o $FOLDER/metadata \
+        $MAINDIR/fr \
+        -o $MAINDIR/metadata \
         --ignore_if_exists \
         -c yt_scrape_check.tsv \
         -p yt_scrape_metadata.json || exit $?
 
     echo "Measure timing..."
-    python3 ../total_duration.py YouTubeFr/mp3
+    python3 ../total_duration.py $MAINDIR
     sleep 3600
 
 done
