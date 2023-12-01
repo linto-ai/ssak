f183b7bda509db0a08c590254ea9e316a0028f8f
hedi naouara <hnaouara@gmail.com>
2023-12-01 16:00:33 +0100

    fix the stm2kaldi.py script

--- tools/kaldi/stm2kaldi.py
+++ tools/kaldi/stm2kaldi.py
@@ -33,86 +33,80 @@ def stm_to_dataframe(stm_file):
     stmframe['filename']
     # Extract gender information
     stmframe["gender"] = stmframe["gender"].str.extract(r'<\S+,\S+,(.*?)>').replace('female', 'f').replace('male', '')           
-               
+    stmframe = stmframe.sort_values(by=['speaker_id'])
+           
     return stmframe
 
-def stmframe_to_kaldi(stm_file, audio_path, output_folder):
+def stmframe_to_kaldi(stm_file, audio_path, output_folder, prefix=None):
     stmframe = stm_to_dataframe(stm_file)
     if os.path.isdir(output_folder):
         raise RuntimeError(f"Output folder {output_folder} already exists. Please remove it to overwrite.")
     os.makedirs(output_folder,exist_ok=True)
-    utt2spk = []
-    spk2utt = []
-    text = []
-    wav_scp = []
-    segments = []
-    spk2gender = []
-    utt2dur = []
     
-    for index, row in stmframe.iterrows():
-        utt_id = row['filename'].split('.')[0]
-        uniq_utt_id = f'{utt_id}_{index}'
-        spk_id = f"{row['speaker_id']}_{index}"
-        transcription = row['transcription']
-        if transcription is None:
-            raise RuntimeError(f"text is non for {uniq_utt_id} utt!!!")
+    unique_wav_paths = set()
+
+    with open(f'{output_folder}/utt2spk', 'w', encoding='utf-8') as utt2spk, \
+            open(f'{output_folder}/spk2utt', 'w', encoding='utf-8') as spk2utt,\
+            open(f'{output_folder}/text', 'w', encoding='utf-8') as text, \
+            open(f'{output_folder}/wav.scp', 'w', encoding='utf-8') as wav_scp, \
+            open(f'{output_folder}/segments', 'w', encoding='utf-8') as segments, \
+            open(f'{output_folder}/utt2dur', 'w', encoding='utf-8') as utt2dur, \
+            open(f'{output_folder}/spk2gender', 'w', encoding='utf-8') as spk2gender:
         
-        # Assuming 'filename' contains the path to the audio file
-        audio_name = os.path.realpath(os.path.join(audio_path, row['filename']))
-        assert audio_name,f'This Audio file {row["filename"]} is not exist in {audio_path}'
-           
-        # Write utt2spk
-        utt2spk.append(f'{uniq_utt_id} {spk_id}')
-        spk2utt.append(f'{spk_id} {uniq_utt_id}')
-        # Write text
-        text.append(f'{uniq_utt_id} {transcription}')
-
-        # Write wav.scp
-        wav_scp.append(f'{uniq_utt_id} sox {audio_name} -t wav -r 16k -b 16 -c 1 - |')
-
-            # Assuming 'start' and 'end' columns are available
-        if not pd.isna(row['start']) and not pd.isna(row['end']):
-            start_time = row['start']
-            end_time = row['end']
-            duration = float(row['end']) - float(row['start'])
-            if duration == 0:
-                raise RuntimeError(f"Audio file not found for {utt_id} (in {audio_path})")
-            # Write segments
-            segments.append(f'{uniq_utt_id} {utt_id} {start_time} {end_time}')
-            utt2dur.append(f'{uniq_utt_id} {duration}')
+        for index, row in stmframe.iterrows():
+            utt_id = row['filename'].split('.')[0]
+            uniq_utt_id = f'{utt_id}-seg_{index:04d}'
+            spk_id = f"{row['speaker_id']}-seg_{index:04d}"
+            if prefix:
+                spk_id = prefix + spk_id
+                uniq_utt_id = prefix + uniq_utt_id
+            transcription = row['transcription']
             
-        # Assuming 'gender' column is available
-        if 'gender' in stmframe.columns:
-            if row.get("gender") == "other":
-                row["gender"] = random.choice(["m", "f"])
-            gender = row['gender']
-            # Write spk2gender
-            spk2gender.append(f'{spk_id} {gender}')
-        else:
-            gender = random.choice(["m", "f"])
-            spk2gender.append(f'{spk_id} {gender}')
-        
-    # Save the generated data to files
-    with open(f'{output_folder}/utt2spk', 'w') as f:
-        f.write('\n'.join(utt2spk))
+            if transcription is None:
+                raise RuntimeError(f"text is non for {uniq_utt_id} utt!!!")
+            
+            # Assuming 'filename' contains the path to the audio file
+            audio_name = os.path.realpath(os.path.join(audio_path, row['filename']))
+            assert audio_name,f'This Audio file {row["filename"]} is not exist in {audio_path}'
+            
+            # Write utt2spk
+            utt2spk.write(f'{uniq_utt_id} {spk_id}\n')
+            spk2utt.write(f'{spk_id} {uniq_utt_id}\n')
+            # Write text
+            text.write(f'{uniq_utt_id} {transcription}\n')
 
-    with open(f'{output_folder}/text', 'w') as f:
-        f.write('\n'.join(text))
+            
 
-    with open(f'{output_folder}/wav.scp', 'w') as f:
-        f.write('\n'.join(wav_scp))
-    if segments:
-        with open(f'{output_folder}/segments', 'w') as f:
-            f.write('\n'.join(segments))
-    if utt2dur:
-        with open(f'{output_folder}/utt2dur', 'w') as f:
-            f.write('\n'.join(utt2dur))
-    if spk2gender:
-        with open(f'{output_folder}/spk2gender', 'w') as f:
-            f.write('\n'.join(spk2gender))
-    
+                # Assuming 'start' and 'end' columns are available
+            if not pd.isna(row['start']) and not pd.isna(row['end']):
+                start_time = round(float(row['start']),3)
+                end_time = round(float(row['end']), 3)
+                duration = round(end_time - start_time, 3)
+                if duration == 0:
+                    raise RuntimeError(f"Audio file not found for {utt_id} (in {audio_path})")
+                # Write segments
+                segments.write(f'{uniq_utt_id} {utt_id} {start_time} {end_time}\n')
+                utt2dur.write(f'{uniq_utt_id} {duration}\n')
+                
+            # Assuming 'gender' column is available
+            if 'gender' in stmframe.columns:
+                if row.get("gender") == "other":
+                    row["gender"] = random.choice(["m", "f"])
+                gender = row['gender']
+                # Write spk2gender
+                spk2gender.write(f'{spk_id} {gender}\n')
+            else:
+                gender = random.choice(["m", "f"])
+                spk2gender.write(f'{spk_id} {gender}\n') 
+            
+            # Write wav.scp
+            if os.path.exists(audio_name):
+                if utt_id not in unique_wav_paths:
+                    wav_scp.write(f'{utt_id} sox {audio_name} -t wav -r 16k -b 16 -c 1 - |\n')
+                    unique_wav_paths.add(utt_id)
     # check_kaldi_dir(output_folder, language=None)
 
+    # check_kaldi_dir(output_folder)
 if __name__ == '__main__':
 
     import argparse
@@ -121,9 +115,10 @@ if __name__ == '__main__':
     parser.add_argument('stmfile', help='Folder with trs files')
     parser.add_argument('audio_folder', help='Folder with audio files (if different from trs folder)', nargs='?', default=None)
     parser.add_argument('output_folder', help='output directory')
+    parser.add_argument('--prefix', default=None, type=str, help='A prefix for all ids (ex: MyDatasetName_)')
     args = parser.parse_args()
     
     assert os.path.isdir(args.audio_folder), f"Folder {args.audio_folder} does not exist"
     assert not os.path.isdir(args.output_folder), f"Folder {args.output_folder} already exists"
     
-    stmframe_to_kaldi(args.stmfile, args.audio_folder, args.output_folder)
+    stmframe_to_kaldi(args.stmfile, args.audio_folder, args.output_folder,args.prefix)
