ed373fa6748699612e0f76a4a8f1652f5a5b5bae
Jeronymous <jeromelouradour@gmail.com>
2024-06-10 13:40:42 +0200

    better packaging of gender classification

--- tools/kaldi/kaldi_spk2gender.py
+++ tools/kaldi/kaldi_spk2gender.py
@@ -3,8 +3,7 @@ from linastt.utils.audio import load_audio
 from linastt.utils.dataset import kaldi_folder_to_dataset
 from linastt.utils.kaldi import check_kaldi_dir
 import torch
-from transformers import AutoConfig, Wav2Vec2FeatureExtractor
-from linastt.utils.models_gender import HubertForSpeechClassification 
+from linastt.utils.gender import predict_gender 
 from tqdm import tqdm
 import os
 import argparse
@@ -13,35 +12,7 @@ import warnings
 # Ignore all warnings
 # warnings.simplefilter("ignore")
 
-def predict_gender(waveform, feature_extractor, model, config, device="cpu", sample_rate=16_000):
-    inputs = feature_extractor(waveform, sampling_rate=sample_rate, return_tensors="pt", padding=True)
-    if device != "cpu":
-        inputs = inputs.to(device)
-    inputs = {key: inputs[key] for key in inputs}
-
-    with torch.no_grad():
-        logits = model(**inputs).logits
-
-    scores = torch.softmax(logits, dim=1).detach().cpu().numpy()[0]
-
-    # Get the index of the label with the highest score
-    best_index = np.argmax(scores)
-
-    # Get the label and score of the best prediction
-    best_label = config.id2label[best_index]
-    best_score = f"{round(scores[best_index] * 100, 3):.1f}%"
-
-    return {"Label": best_label, "Score": best_score}
-
 def main(args):
-    # Specify device manually
-    device = torch.device(args.device)
-
-    # Load model and configuration
-    model_name_or_path = args.model_path
-    config = AutoConfig.from_pretrained(model_name_or_path)
-    feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_name_or_path)
-    model = HubertForSpeechClassification.from_pretrained(model_name_or_path).to(device)
 
     # Example usage
     kaldi_spk2g_file = os.path.join(args.kaldi_dir, 'spk2gender')
@@ -57,10 +28,10 @@ def main(args):
             start = sample['start']
             end = sample['end']
             waveform = load_audio(audio_path, start=start, end=end)
-            output = predict_gender(waveform, feature_extractor, model, config, device=device)
-            gender =  output['Label']
+            gender = predict_gender(waveform, device=args.device, model=args.model_path, output_type="best")
             gender_f.write(f'{segment_id} {gender.lower()}\n')
             gender_f.flush()
+
     check_kaldi_dir(args.kaldi_dir)
         
 
