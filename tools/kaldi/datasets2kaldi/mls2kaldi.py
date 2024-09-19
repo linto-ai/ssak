2f217ea4fa0336fe0087caff4032ae14ce043180
Audran Bert <bert.audran@gmail.com>
2024-09-19 15:36:59 +0200

    update mls2kaldi

--- tools/kaldi/datasets2kaldi/mls2kaldi.py
+++ tools/kaldi/datasets2kaldi/mls2kaldi.py
@@ -19,9 +19,9 @@ if __name__=="__main__":
     
     output_path = args.output
     
-    raw_dev = os.path.join(output_path, "others/dev")
-    raw_test = os.path.join(output_path, "others/test")
-    raw_train = os.path.join(output_path, "others/train")
+    raw_dev = os.path.join(output_path, "raw/dev")
+    raw_test = os.path.join(output_path, "raw/test")
+    raw_train = os.path.join(output_path, "raw/train")
     
     nocasepunc_dev = os.path.join(output_path, "nocasepunc/dev")
     nocasepunc_test = os.path.join(output_path, "nocasepunc/test")
